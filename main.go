56ab72dc0b39218fb778d1a2076c73a791b1ef9c
gyulaieric <gyulaieric@gmail.com>
2025-12-21 15:38:17 +0200

    implemented metrics

--- main.go
+++ main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	})
+}
+
+func (cfg *apiConfig) handlerResetMetrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		r.Header.Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Metrics have been reset successfully"))
+	})
+}
 
 func main() {
 	port := "8080"
 	filepathRoot := http.Dir(".")
 
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(filepathRoot)))
+	mux.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(
+			http.StripPrefix(
+				"/app",
+				http.FileServer(filepathRoot),
+			),
+		),
+	)
 	mux.HandleFunc("/healthz", handlerHealthz)
+	mux.Handle("/metrics", apiCfg.handlerMetrics())
+	mux.Handle("/reset", apiCfg.handlerResetMetrics())
 
 	server := http.Server{
 		Addr:    ":" + port,
