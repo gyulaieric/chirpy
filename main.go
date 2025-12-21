f6b733b1cd7524e3a8556dac2a57a780daf8b11e
gyulaieric <gyulaieric@gmail.com>
2025-12-21 15:07:06 +0200

    added readiness endpoint

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	filepathRoot := http.Dir(".")
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(filepathRoot))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(filepathRoot)))
+	mux.HandleFunc("/healthz", handlerHealthz)
 
 	server := http.Server{
 		Addr:    ":" + port,
@@ -18,4 +19,10 @@ func main() {
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	r.Header.Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
